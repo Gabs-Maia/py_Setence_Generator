1d1bab8c3e49838e576c502b9b4c0dd16d189434
Gabs-Maia <gabrielsouzamaia99@gmail.com>
2023-10-19 03:26:05 -0300

    hi

--- pdf-auto.py
+++ pdf-auto.py
@@ -9,10 +9,10 @@ reader = PdfReader(input_path)
 def modify_pdf(input_path, output_path):
     # Open the input PDF file in read binary mode
     with open(input_path, "rb") as input_file:
-        # Create a PDF reader object
+        # Create a PDF reader OBJ
         pdf_reader = PdfReader(input_file)
         
-        # Get the number of pages in the input PDF
+        # Get number of pages 
         num_pages = len(pdf_reader.pages)
         print(f"Number of pages: {num_pages}")
         
@@ -31,6 +31,7 @@ def modify_pdf(input_path, output_path):
         with open(output_path, "wb") as output_file:
         # Write the output PDF to the file
             pdf_writer.write(output_file)
+
     except PermissionError:
     
         print(f"Permission error: Unable to write to {output_path}")
@@ -44,6 +45,16 @@ pdf = PdfReader(open(pdf_file, 'rb'))
 text = ''
 for page in pdf.pages:
     text += page.extract_text()
+
+def Words_Freq(text):
+
+    words = re.findall(r'\b\w+\b', text)
+    word_counts = Counter(words)
+
+    sorted_word_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+    convert_to_string = ''.join(str(x) for x in sorted_word_counts)
+    
+    return convert_to_string    
       
 # Create a frequency list of German words
 words = re.findall(r'\b\w+\b', text)
@@ -59,8 +70,6 @@ output_path = input_path
 modify_pdf(input_path, output_path)
 
 
-# Example usage
-#input_path = r"C:\Users\gabri\OneDrive\Área de Trabalho\Conhecimento\Romance and Fiction\German\Ali-Hazelwood-Die-theoretische-Unwahrscheinlichkeit-von-Liebe-–-Die-deutsche-Ausgabe-von-»The-Love-H.pdf"
 
 
 
