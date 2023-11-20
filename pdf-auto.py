41a2f3d9cc91f54bb6d4acfffc3231dfe29e3c1a
Gabs-Maia <gabrielsouzamaia99@gmail.com>
2023-11-19 21:57:26 -0300

    fgfggg

--- pdf-auto.py
+++ pdf-auto.py
@@ -1,22 +1,5 @@
-from format_text import modify_pdf, words_freq
-from PyPDF2 import PdfReader, PdfWriter
-from collections import Counter
-import re
+import requests as re
 
-input_path = input("Enter the file path: ")
+response = re.get(url)
+json = response.json
 
-# Open the PDF file
-pdf = PdfReader(input_path)
-
-text = ''
-for page in pdf.pages:
-    text += page.extract_text()
-
-convert_to_string = words_freq(text)
-      
-with open('frequency_list.txt', 'a') as frequency_list:
-    frequency_list.write(convert_to_string)
-
-output_path = "output.pdf"
-
-modify_pdf(input_path, output_path)
