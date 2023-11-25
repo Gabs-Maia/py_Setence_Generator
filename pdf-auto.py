11f3de8e20303eef47026cefb61aa4864c432929
Gabs-Maia <gabrielsouzamaia99@gmail.com>
2023-11-25 16:08:03 -0300

    go!!

--- pdf-auto.py
+++ pdf-auto.py
@@ -1,5 +1,23 @@
 import requests as re
+import json 
+from bs4 import BeautifulSoup as bs
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
-response = re.get(url)
-json = response.json
+
+def get_text():
+    # Download HTML content
+    url = input("Enter the desired url -: ")
+    fetch_data = re.get(url)
+    html = fetch_data.content
+
+    #Parse fetched HTML 
+    soup = BeautifulSoup(html, "html.parser")
+    text = soup.get_text()
+    
+    #Create PDF
+    output = PdfFileWriter()
+
+    #Convert to PDF :D 
+
+    onvert = output.addPage(PdfFileReader(BytesIO(text.encode("utf-8"))))
 
